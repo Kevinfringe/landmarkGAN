d73e4eb57d78ae322f5c6bbd9e82aaf00b59c753
Kevien Lee <u6988392@anu.edu.au>
2023-03-28 07:57:58 +0800

    Initial commit

---
+++ data_preprocess/jaffe_processor.py
@@ -0,0 +1,12 @@
+'''
+    This file is to pair up the specific image pairs.
+    eg:
+    NE -> OTHER EXPRESSIONS.
+'''
+
+import os
+import cv2
+import numpy as np
+
+path = os.getcwd()
+print(path)
